7a2f001ee3d8e7c3c31e1ecf9543e8c774026bf1
criticalzero123 <jdcaramonte159@gmail.com>
2023-04-12 15:29:26 +0800

    Add: Stripe Payment Intent

---
+++ Controllers/PaymentController.cs
@@ -0,0 +1,43 @@
+﻿using ConstradeApi.Model.Response;
+using ConstradeApi.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Stripe;
+
+namespace ConstradeApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class PaymentController : ControllerBase
+    {
+
+        private readonly IStripeClient client;
+        public PaymentController()
+        {
+            client = new StripeClient(StripeSecretKey.GetStripeSecretKey());
+        }
+
+        [HttpPost("stripe/create-payment-intent")]
+        public async Task<IActionResult> CreatePaymentIntent(long amount)
+        {
+            try
+            {
+                var options = new PaymentIntentCreateOptions
+                {
+                    Amount = amount*100,
+                    Currency = "PHP",
+                };
+                var service = new PaymentIntentService(client);
+                var paymentIntent = await service.CreateAsync(options);
+
+                return Ok(ResponseHandler.GetApiResponse(ResponseType.Success, new { paymentIntent.ClientSecret }));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ResponseHandler.GetExceptionResponse(ex));
+            }
+        }
+    }
+}
---
+++ Services/StripeSecretKey.cs
@@ -0,0 +1,22 @@
+﻿namespace ConstradeApi.Services
+{
+    public class StripeSecretKey
+    {
+        private readonly IConfiguration _configuration;
+        private string KEY { get; set; }
+        public StripeSecretKey()
+        {
+            _configuration = new ConfigurationBuilder()
+            .AddUserSecrets<StripeSecretKey>()
+            .Build();
+
+            KEY = _configuration["StripeSecretKey"]!;
+        }
+
+        public static string GetStripeSecretKey()
+        {
+            StripeSecretKey sp = new StripeSecretKey();
+            return sp.KEY;
+        }
+    }
+}
